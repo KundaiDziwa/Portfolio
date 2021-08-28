899776442562c5d3821f82c9430f31c60b61d1c0
Kundai Dziwa <kundingedziwa@Kundais-MBP.cg.shawcable.net>
2021-08-28 01:49:05 -0600

    Introduction and foundation to 3d functionality and threejs complete.

--- src/home.js
+++ src/home.js
@@ -1,4 +1,322 @@
 import * as THREE from 'https://cdn.skypack.dev/three';
+
+// Global Variables
+const vehicleColors = [0xa52523, 0xbdb638, 0x78b14b];
+
+// Helper Functions
+function pickRandom(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
 const scene = new THREE.Scene();
-console.log(scene);
-//Start
+
+const playerCar = Car();
+scene.add(playerCar);
+
+/* ----- Building the Track ----- */
+const trackRadius = 225;
+const trackWidth = 45;
+const innerTrackRadius = trackRadius - trackWidth;
+const outerTrackRadius = trackRadius + trackWidth;
+
+const arcAngle1 = (1 / 3) * Math.PI; // 60 degrees
+
+const deltaY = Math.sin(arcAngle1) * innerTrackRadius;
+const arcAngle2 = Math.asin(deltaY / outerTrackRadius);
+
+const arcCenterX = (Math.cos(arcAngle1) * innerTrackRadius + Math.cos(arcAngle2) * outerTrackRadius) / 2;
+
+const arcAngle3 = Math.acos(arcCenterX / innerTrackRadius);
+
+const arcAngle4 = Math.acos(arcCenterX / outerTrackRadius);
+
+/* ----- Setting up lights ----- */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
+dirLight.position.set(100, -300, 400);
+scene.add(dirLight);
+
+/* ----- Setting up camera ----- */
+const aspectRatio = window.innerWidth / window.innerHeight;
+const cameraWidth = 950;
+const cameraHeight = cameraWidth / aspectRatio;
+  
+const camera = new THREE.OrthographicCamera(
+    cameraWidth / -2,// left
+    cameraWidth / 2, // right
+    cameraHeight / 2, //top
+    cameraHeight / -2, // bottom
+    0, // near plane
+    1000 // far plane 
+);
+camera.position.set(0, -210, 300);
+camera.lookAt(0, 0, 0);
+
+renderMap(cameraWidth, cameraHeight * 2);
+
+/* ----- Setting up the render ----- */
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+document.body.appendChild(renderer.domElement);
+
+/* ----- Buiding/Constructing the transportation vehicle ----- */
+function Car() {
+    const car = new THREE.Group();
+
+    const color = pickRandom(vehicleColors);
+
+    const main = new THREE.Mesh(
+        new THREE.BoxBufferGeometry(60, 30, 15),
+        new THREE.MeshLambertMaterial({color})
+    );
+    main.position.z = 12;
+    car.add(main);
+
+    const carFrontTexture = getCarFrontTexture();
+    carFrontTexture.center = new THREE.Vector2(0.5, 0.5);
+    carFrontTexture.rotation = Math.PI / 2;
+
+    const carBackTexture = getCarFrontTexture();
+    carBackTexture.center = new THREE.Vector2(0.5, 0.5);
+    carBackTexture.rotation = -Math.PI / 2;
+
+    const carRightSideTexture = getCarSideTexture();
+
+    const carLeftSideTexture = getCarSideTexture();
+    carLeftSideTexture.flipY = false;
+
+    const cabin = new THREE.Mesh(new THREE.BoxBufferGeometry(33, 24, 12), [
+        new THREE.MeshLambertMaterial({map: carFrontTexture}),
+        new THREE.MeshLambertMaterial({map: carBackTexture}),
+        new THREE.MeshLambertMaterial({map: carLeftSideTexture}),
+        new THREE.MeshLambertMaterial({map: carRightSideTexture}),
+        new THREE.MeshLambertMaterial({color: 0xffffff}), // top
+        new THREE.MeshLambertMaterial({color: 0xffffff}), // bottom
+    ]);
+    cabin.position.x = -6;
+    cabin.position.z = 25.5;
+    car.add(cabin);
+
+    const backWheel = wheel();
+    backWheel.position.z = 6;
+    backWheel.position.x = -18;
+    car.add(backWheel);
+
+    const frontWheel = wheel();
+    frontWheel.position.z = 6;
+    frontWheel.position.x = 18;
+    car.add(frontWheel);
+
+    return car;
+}
+// Car Build Helper Functions
+function wheel() {
+    const wheel = new THREE.Mesh(
+        new THREE.BoxBufferGeometry(12, 33, 12),
+        new THREE.MeshLambertMaterial({color: 0x333333})
+    );
+    return wheel;
+}
+function getCarFrontTexture() {
+    const canvas = document.createElement("canvas");
+    canvas.width = 64;
+    canvas.height = 32;
+    const context = canvas.getContext("2d");
+
+    context.fillStyle = "#ffffff";
+    context.fillRect(0, 0, 64, 32);
+
+    context.fillStyle = "#666666";
+    context.fillRect(8, 8, 48, 24);
+
+    return new THREE.CanvasTexture(canvas);
+}
+function getCarSideTexture() {
+    const canvas = document.createElement("canvas");
+    canvas.width = 128;
+    canvas.height = 32;
+    const context = canvas.getContext("2d");
+
+    context.fillStyle = "#ffffff";
+    context.fillRect(0, 0, 128, 32);
+
+    context.fillStyle = "#666666";
+    context.fillRect(10, 8, 38, 24);
+    context.fillRect(58, 8, 60, 24);
+
+    return new THREE.CanvasTexture(canvas);
+}
+
+function getLineMarkings(mapWidth, mapHeight) {
+    const canvas = document.createElement("canvas");
+    canvas.width = mapWidth;
+    canvas.height = mapHeight;
+    const context = canvas.getContext("2d");
+
+    context.fillStyle = "#546E90";
+    context.fillRect(0, 0, mapWidth, mapHeight);
+
+    context.lineWidth = 2;
+    context.strokeStyle = "#E0FFFF";
+    context.setLineDash([10, 14]);
+
+    // Left circle
+    context.beginPath();
+    context.arc(
+        mapWidth / 2 - arcCenterX,
+        mapHeight / 2,
+        trackRadius,
+        0,
+        Math.PI * 2
+    );
+    context.stroke();
+
+    // Right circle 
+    context.beginPath();
+    context.arc(
+        mapWidth / 2 + arcCenterX,
+        mapHeight / 2,
+        trackRadius,
+        0,
+        Math.PI * 2
+    );
+    context.stroke();
+
+    return new THREE.CanvasTexture(canvas);
+}
+
+/* ----- Building the Map ----- */ 
+function renderMap(mapWidth, mapHeight) {
+    // Plane with line markings
+    const lineMarkingsTexture = getLineMarkings(mapWidth, mapHeight);
+
+    const planeGeometry = new THREE.PlaneBufferGeometry(mapWidth, mapHeight);
+    const planeMaterial = new THREE.MeshLambertMaterial({
+        map: lineMarkingsTexture
+    });
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    scene.add(plane);
+
+    //Extruded geometry
+    const islandLeft = getLeftIsland();
+    const islandRight = getRightIsland();
+    const islandMiddle = getMiddleIsland();
+    const outerField = getOuterField(mapWidth, mapHeight);
+
+    const fieldGeometry = new THREE.ExtrudeBufferGeometry(
+        [islandLeft, islandMiddle, islandRight, outerField],
+        {depth: 6, bevelEnabled: false}
+    );
+
+    const fieldMesh = new THREE.Mesh(fieldGeometry, [
+        new THREE.MeshLambertMaterial({color: 0x67c240}),
+        new THREE.MeshLambertMaterial({color: 0x23311c})
+    ]);
+    scene.add(fieldMesh);
+}
+
+// Building Islands 
+function getLeftIsland() {
+    const islandLeft = new THREE.Shape();
+
+    islandLeft.absarc(
+        -arcCenterX,
+        0,
+        innerTrackRadius,
+        arcAngle1,
+        -arcAngle1,
+        false
+    );
+
+    islandLeft.absarc(
+        arcCenterX,
+        0,
+        outerTrackRadius,
+        Math.PI + arcAngle2,
+        Math.PI - arcAngle2,
+        true
+    );
+
+    return islandLeft;
+}
+function getMiddleIsland() {
+    const islandMiddle = new THREE.Shape();
+
+    islandMiddle.absarc(
+        -arcCenterX,
+        0,
+        innerTrackRadius,
+        arcAngle3,
+        -arcAngle3,
+        true
+    );
+
+    islandMiddle.absarc(
+        arcCenterX,
+        0,
+        innerTrackRadius,
+        Math.PI + arcAngle3,
+        Math.PI - arcAngle3,
+        true
+    );
+
+    return islandMiddle;
+}
+function getRightIsland() {
+    const islandRight = new THREE.Shape();
+
+    islandRight.absarc(
+        arcCenterX,
+        0,
+        innerTrackRadius,
+        Math.PI - arcAngle1,
+        Math.PI + arcAngle1,
+        true
+    );
+
+    islandRight.absarc(
+        -arcCenterX,
+        0,
+        outerTrackRadius,
+        -arcAngle2,
+        arcAngle2,
+        false
+    );
+
+    return islandRight;
+}
+function getOuterField(mapWidth, mapHeight) {
+    const field = new THREE.Shape();
+
+    field.moveTo(-mapWidth / 2, -mapHeight / 2);
+    field.lineTo(0, -mapHeight / 2);
+
+    field.absarc(
+        -arcCenterX,
+        0,
+        outerTrackRadius,
+        -arcAngle4,
+        arcAngle4,
+        true
+    );
+
+    field.absarc(
+        arcCenterX,
+        0,
+        outerTrackRadius,
+        Math.PI - arcAngle4,
+        Math.PI + arcAngle4,
+        true
+    );
+
+    field.lineTo(0, -mapHeight / 2);
+    field.lineTo(mapWidth / 2, -mapHeight / 2);
+    field.lineTo(mapWidth / 2, mapHeight / 2);
+    field.lineTo(-mapWidth / 2, mapHeight / 2);
+
+    return field;
+}
